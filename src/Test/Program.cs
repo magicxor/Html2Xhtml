0cc0d5d8c0eb401a156355872de6801f08db3e07
Ilia Burakov <burakov.ilia@gmail.com>
2021-12-15 06:26:32 +0800

    1.1.2 (2010-05-05)

---
+++ src/Test/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+using Corsis.Xhtml;
+
+namespace Test
+{
+    class Program
+    {
+        static void ReportEncodings(string desc, IEnumerable<string> encodings)
+        {
+            Console.WriteLine("## {0} ##", desc);
+            foreach (var encoding in encodings.OrderBy(a => a))
+                Console.WriteLine(encoding);
+        }
+
+        static void Main(string[] args)
+        {
+            HashSet<string> dotNetOnly, iconvOnly, both;
+            Html2Xhtml.SupportedEncodings.GetSupportSets(out both, out dotNetOnly, out iconvOnly);
+            ReportEncodings(" both  ", both);
+            ReportEncodings(" .net  ", dotNetOnly);
+            ReportEncodings(" iconv ", iconvOnly);
+
+
+
+            Console.WriteLine(Html2Xhtml.SupportedEncodings.Contains("UTF-8"));
+
+            Html2XhtmlBasicExample();
+            Html2XDocumentExample1();
+            Html2XDocumentExample2();
+            Html2XDocumentExample3();
+
+            Console.ReadKey(true);
+        }
+
+        static string html = "<html><head><TITLE>title</TITLE></head><body>I♥NY☢<p>b<br>c: <img src=2 nonsense=x></a></body></html>";
+
+        static void Html2XhtmlBasicExample()
+        {
+            var xhtml = Html2Xhtml.RunAsFilter(stdin => stdin.Write(html), compactBlockElements: true).ReadToEnd();
+            Console.WriteLine(xhtml);
+            File.WriteAllText("1.out", xhtml);
+        }
+
+        static void Html2XDocumentExample1()
+        {
+            var xhtml = Html2Xhtml.RunAsFilter(stdin => stdin.Write(html), compactBlockElements: true).ReadToEnd();
+            var xml   = XhtmlNamedEntities.NameToNumber.Map(xhtml);
+            var xdoc  = XDocument.Parse(xml);
+            Console.WriteLine(xdoc);
+            File.WriteAllText("2.out", xdoc.ToString());
+        }
+
+        static void Html2XDocumentExample2()
+        {
+            var xhtml = Html2Xhtml.RunAsFilter(stdin => stdin.Write(html), compactBlockElements: true).ReadToEnd();
+            var xml1  = XhtmlNamedEntities.NameToNumber.Map(xhtml);
+            var xml2  = Html2XhtmlExtensions.RipNamespace(xml1);
+            var xdoc  = XDocument.Parse(xml2);
+            Console.WriteLine(xdoc);
+            File.WriteAllText("3.out", xdoc.ToString());
+        }
+
+        static void Html2XDocumentExample3()
+        {
+            var xdoc = Html2Xhtml.RunAsFilter(stdin => { Console.ReadKey(true); stdin.Write(html); }, Html2Xhtml.DocType.Xhtml_1_1);//.ReadToXDocument();
+            Console.WriteLine(xdoc);
+            File.WriteAllText("4.out", xdoc.ToString());
+        }
+    }
+}
---
+++ src/WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Corsis.Xhtml;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string html = "<html><head><TITLE>title</TITLE></head><body>I♥NY☢<p>b<br>c: <img src=2 nonsense=x></a></body></html>";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void runButton_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = Html2Xhtml.RunAsFilter(stdin => stdin.Write(html)).ReadToXDocument().ToString();
+        }
+    }
+}
