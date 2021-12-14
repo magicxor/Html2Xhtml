54bb498b61623e9ebb0de58f141560d21f9b904a
Ilia Burakov <burakov.ilia@gmail.com>
2021-12-15 06:23:51 +0800

    1.1.2-4 (2010-07-11)

---
+++ src/WpfTest/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfTest
+{
+    public static class Extensions
+    {
+        public static Stream OpenDownload(this Uri address)
+        {
+            var web = new WebClient();
+            web.Headers["Referer"] = "http://www.google.com/";
+            web.Headers["Accept"] = "application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
+            web.Headers["User-Agent"] = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.2 (KHTML, like Gecko) Chrome/6.0.453.1 Safari/534.2";
+            web.Headers["Accept-Language"] = "en-US,en;q=0.8";
+            web.Headers["Accept-Charset"] = "utf-8,ISO-8859-1;q=0.7,*;q=0.3";
+            return web.OpenRead(address);
+        }
+    }
+}
---
+++ src/WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.IO;
+
+using Corsis.Xhtml;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void runButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var xhtml = Html2Xhtml.RunAsFilter(
+                    i => i.Write(htmlBox.Text),
+                    outputDocType: (DocType)Enum.Parse(typeof(DocType), s.doc.SelectedValue.ToString()),
+                    e: s.e.IsChecked.Value,
+                    inputEncodingName: s.inputEncoding.Text,
+                    outputEncodingName: s.outputEncoding.Text,
+                    lineLength: int.Parse(s.lineLength.Text),
+                    tabLength: int.Parse(s.tabLength.Text),
+                    preserveWhitespaceInComments: s.spaceComment.IsChecked.Value,
+                    noProtectCData: !s.cdata.IsChecked.Value,
+                    compactBlockElements: s.block.IsChecked.Value,
+                    emptyElementTagsAlways: s.emptyAlways.IsChecked.Value,
+                    compactEmptyElementTags: s.empty.IsChecked.Value,
+                    dosEndOfLine: s.dos.IsChecked.Value
+                    ).ReadToEnd();
+
+                xhtmlBox.Text = xhtml;
+                xmlBox.Text = xhtml.ToXDocument().ToString(SaveOptions.DisableFormatting);
+            }
+            catch (Exception ex)
+            {
+                xhtmlBox.Text = ex.Message;
+            }
+        }
+
+        private void getButton_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri(uriBox.Text);
+            try
+            {
+                using (var r = uri.OpenDownload())
+                {
+                    htmlBox.Text = new StreamReader(r).ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                htmlBox.Text = ex.Message;
+            }
+        }
+    }
+}
